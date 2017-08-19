26a62544759c2f38db18c7c4dd4dfd02f1e7c370
konatasick <konatasick@gmail.com>
2017-08-19 12:13:50 +0800

    Add pool index in the end

---
+++ js/index.js
@@ -0,0 +1,32 @@
+﻿var myObj, i, j, x = "";
+myObj = {
+    "name": "index",
+    "num": 17,
+    "sites": [
+        { "name": "[600wDL·斯卡哈]", "info": "600w.html" },
+        { "name": "[鬼岛·源赖光]", "info": "onigashima.html" },
+        { "name": "[第六章1·拉二]", "info": "sixth.html" },
+        { "name": "[夏日祭·达芬奇]", "info": "1st.html" },
+        { "name": "[泳装1·枪玉]", "info": "summer1.html" },
+        { "name": "[泳装2·弓呆]", "info": "summer2.html" },
+        { "name": "[第六章2·白枪呆]", "info": "sixth2.html" },
+        { "name": "[魔法少女·伊莉雅]", "info": "prisma.html" },
+        { "name": "[尼禄祭再临·花嫁尼禄]", "info": "nero2.html" },
+        { "name": "[700wDL·黑贞]", "info": "700w.html" },
+        { "name": "[万圣节复刻·玉藻前]", "info": "halloween1.html" },
+        { "name": "[新万圣节·艳后]", "info": "halloween2.html" },
+        { "name": "[Fate/EXTELLA]", "info": "extella.html" },
+        { "name": "[圆桌骑士]", "info": "knights.html" },
+        { "name": "[圣诞节复刻·杰克]", "info": "xmas1.html" },
+        { "name": "[天草四郎]", "info": "shiro.html" },
+        { "name": "[新圣诞节·弓凛]", "info": "xmas2.html" }
+    ]
+}
+
+for (i in myObj.sites) {
+    x += "<a href=" + myObj.sites[i].info + ">" + myObj.sites[i].name + "</a>";
+
+}
+
+document.getElementById("index").innerHTML = x;
+ 
