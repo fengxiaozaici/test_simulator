48abed096926181f8195f86b5845310507a117e5
konatasick <konatasick@gmail.com>
2017-09-01 14:34:26 +0800

    Add seventh

--- js/index.js
+++ js/index.js
@@ -20,7 +20,8 @@ myObj = {
         { "name": "[圣诞节复刻·杰克]", "info": "xmas1.html" },
         { "name": "[天草四郎]", "info": "shiro.html" },
         { "name": "[新圣诞节·弓凛]", "info": "xmas2.html" },
-        { "name": "[阿蒂拉]", "info": "altila.html" }
+        { "name": "[阿蒂拉]", "info": "altila.html" },
+        { "name": "[第七章1·恩奇都]", "info": "seventh.html" }
     ]
 }
 
