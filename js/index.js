fac38b358157ad1b775627c81807d109859a92f7
JOY ZHOU <JOY ZHOU>
2017-10-24 15:36:17 +0800

    Add meiji

--- js/index.js
+++ js/index.js
@@ -34,7 +34,8 @@ myObj = {
         { "name": "[新宿2·黑A]", "info": "shinjuku2.html" },
         { "name": "[白情复刻·天草四郎]", "info": "white1.html" },
         { "name": "[新白情·旧剑]", "info": "white2.html" },
-        { "name": "[本能寺复刻·冲田总司]", "info": "honnouji.html" }
+        { "name": "[本能寺复刻·冲田总司]", "info": "honnouji.html" },
+        { "name": "[明治维新·土方岁三]", "info": "meiji.html" }
     ]
 }
 
